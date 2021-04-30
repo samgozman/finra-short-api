ed4849edd8cec10a63d2abfa93e84900cf4cce66
samgozman <sam@gozman.space>
2021-04-30 23:01:28 +0300

    Project init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'es2021': true,
+		'mongo': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'warn',
+			'tab'
+		],
+		'linebreak-style': [
+			'warn',
+			'unix'
+		],
+		'quotes': [
+			'warn',
+			'single'
+		],
+		'semi': [
+			'warn',
+			'never'
+		]
+	}
+}
