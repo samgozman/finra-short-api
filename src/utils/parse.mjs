df08fcfa806de9504b8742e2973103ef25ff8d2d
samgozman <sam@gozman.space>
2021-05-01 11:48:26 +0300

    Add getDataFromFile()

--- src/utils/parse.mjs
+++ src/utils/parse.mjs
@@ -48,10 +48,40 @@ const getLinksToFiles = async (url) => {
     }
 }
 
+const getDataFromFile = async (url) => {
+    const response = await got(url)
+    const text = response.body.toString()
+    let textArray = text.split(/\r?\n/)
+
+    // Remove first and last line of the array
+    textArray.shift()
+    textArray.pop()
+    textArray.pop()
+
+    let obj = {}
+    textArray.forEach((str) => {
+        // String format: Date|Symbol|ShortVolume|ShortExemptVolume|TotalVolume|Market
+        const strArr = str.split('|')
+        obj[strArr[1]] = {
+            date: strArr[0],
+            shortVolume: strArr[2],
+            shortExemptVolume: strArr[3],
+            totalVolume: strArr[4],
+            nyse: /N/g.test(strArr[5]),
+            nasdaqCarteret: /Q/g.test(strArr[5]),
+            nasdaqChicago: /B/g.test(strArr[5]),
+            adf: /D/g.test(strArr[5])
+        }
+    })
+    
+    return obj
+}
+
 const main = async () => {
     console.log(await getMonthlyPages())
     console.log('')
     console.log(await getLinksToFiles('http://regsho.finra.org/regsho-Index.html'))
+    console.log(await getDataFromFile('http://regsho.finra.org/CNMSshvol20210401.txt'))
 }
 
 main()
