f5e9721ca882b9ef06e4cd8e4b47d7cc64d7f236
samgozman <sam@gozman.space>
2021-05-01 10:36:32 +0300

    Add getLinksToFiles()

--- src/utils/parse.mjs
+++ src/utils/parse.mjs
@@ -3,23 +3,20 @@ import cheerio from 'cheerio'
 
 const getMonthlyPages = async () => {
     try {
-        const currentLink = 'http://regsho.finra.org/regsho-Index.html'
-        const response = await got(currentLink)
+        const baseUrl = 'http://regsho.finra.org/regsho-Index.html'
+        const response = await got(baseUrl)
         const $ = cheerio.load(response.body)
 
         const menuTable = $('body > table:nth-child(2) > tbody > tr > td')
-        const links = Array.prototype.map.call(menuTable, (td) => {
+        let links = {}
+        Array.prototype.map.call(menuTable, (td) => {
             const link = $(td).find('a')
             if (link.text()) {
-                return {
-                    [link.text()]: link.attr('href')
-                }
+                links[link.text()] = link.attr('href')
             } else {
                 // Last / current menu item is empty. 
                 const currentPageDate = $('body > table:nth-child(2) > tbody > tr > td').last().text()
-                return {
-                    [currentPageDate]: currentLink
-                }
+                links[currentPageDate] = baseUrl
             }
         })
 
@@ -31,8 +28,30 @@ const getMonthlyPages = async () => {
     }
 }
 
+const getLinksToFiles = async (url) => {
+    try {
+        const response = await got(url)
+        const $ = cheerio.load(response.body)
+
+        const filePathsNode = $('ul').first().find('li > a')
+        let filePaths = {}
+        Array.prototype.map.call(filePathsNode, (a) => {
+            const link = $(a)
+            filePaths[link.text()] = link.attr('href')
+        })
+
+        return filePaths
+    } catch (error) {
+        return {
+            error: error.message
+        }
+    }
+}
+
 const main = async () => {
     console.log(await getMonthlyPages())
+    console.log('')
+    console.log(await getLinksToFiles('http://regsho.finra.org/regsho-Index.html'))
 }
 
 main()
