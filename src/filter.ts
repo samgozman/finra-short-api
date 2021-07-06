c2aa125311decf3983a21ce00083469c8804cb04
samgozman <sam@gozman.space>
2021-07-06 19:56:56 +0300

    Volume filters class

--- src/filter.ts
+++ src/filter.ts
@@ -1,9 +1,7 @@
 import { ObjectId } from 'mongoose';
 import { Tinkoff } from 'tinkoff-api-securities';
 import { IFilter, Filter } from './models/Filter';
-import { StockPopulatedDocument } from './models/PopulatedVolume';
 import { Stock } from './models/Stock';
-import round from './utils/round';
 
 /** Filter keys */
 type Filters = keyof IFilter & string;
@@ -59,9 +57,56 @@ export async function getFilter(...keys: Filters[]): Promise<ObjectId[]> {
     return ids;
 }
 
+interface FilterUnit {
+    filter: Filters;
+    update(): Promise<void>;
+    get(): Promise<ObjectId[]>;
+}
+
+class VolumeFilter implements FilterUnit {
+    constructor(
+        public filter: Filters,
+        public volType: 'shortVolume' | 'shortExemptVolume' | 'totalVolume',
+        public momentum: 'growing' | 'decreasing',
+        public period: number = 5,
+        public ratio: boolean = false
+    ) {}
+
+    /** Update filters in MongoDB */
+    async update() {
+        await resetFilter(this.filter);
+        const allIds = await Stock.avalibleTickers();
+        for (const _id of allIds) {
+            const stock = (await Stock.findById(_id))!;
+            const volume = (await stock.getVirtual('volume', this.period + 1, 'desc')).volume;
+
+            // Check if populated volume exists
+            if (volume && volume.length > 1) {
+                const volArr = this.ratio
+                    ? volume.map((e) => e[this.volType] / e.totalVolume).reverse()
+                    : volume.map((e) => e[this.volType]).reverse();
+                // Validate each value is greater / lesser than previous
+                const validation: boolean[] =
+                    this.momentum === 'growing'
+                        ? volArr.map((e, i: number) => volArr[i] > volArr[i - 1])
+                        : volArr.map((e, i: number) => volArr[i] < volArr[i - 1]);
+                // Remove first element of the array (it was used for comparing only)
+                validation.shift();
+                // If all validations in row are true
+                const checker = validation.every((v) => v === true);
+                await updateFilter(_id, this.filter, checker);
+            }
+        }
+    }
+
+    async get(): Promise<ObjectId[]> {
+        return await getFilter(this.filter);
+    }
+}
+
 /* FILTERS SECTION: START */
 
-export async function onTinkoffFIlter() {
+export async function onTinkoff() {
     await resetFilter('onTinkoff');
     const tinkoff = new Tinkoff(process.env.SANDBOX_TOKEN!);
     const onTinkoff = await tinkoff.stocks('USD');
@@ -78,27 +123,23 @@ export async function onTinkoffFIlter() {
     }
 }
 
-export async function shortVolGrowsFilter(limit: number = 5) {
-    await resetFilter('shortVolGrows5D');
-    // increment limit to be able to compare index 1 to index 0
-    limit++;
-    const allIds = await Stock.avalibleTickers();
-    for (const _id of allIds) {
-        const stock = (await Stock.findById(_id))!;
-        const volume = (await stock.getVirtual('volume', limit, 'desc')).volume;
-
-        // Check if populated volume exists
-        if (volume && volume.length > 1) {
-            const shortVolArr = volume.map((e) => e.shortVolume).reverse();
-            // each value is greater than previous
-            const validation: boolean[] = shortVolArr.map((e, i) => shortVolArr[i] > shortVolArr[i - 1]);
-            // Remove first element of the array (it was used for comparing only)
-            validation.shift();
-            // If all validations in row are true
-            const checker = validation.every((v) => v === true);
-            await updateFilter(_id, 'shortVolGrows5D', checker);
-        }
-    }
-}
+export const shortVolGrows5D = new VolumeFilter('shortVolGrows5D', 'shortVolume', 'growing', 5);
+export const shortVolDecreases5D = new VolumeFilter('shortVolDecreases5D', 'shortVolume', 'decreasing', 5);
+export const shortVolRatioGrows5D = new VolumeFilter(
+    'shortVolRatioGrows5D',
+    'shortVolume',
+    'growing',
+    5,
+    true
+);
+export const shortVoRatiolDecreases5D = new VolumeFilter(
+    'shortVoRatiolDecreases5D',
+    'shortVolume',
+    'decreasing',
+    5,
+    true
+);
+export const totalVolGrows5D = new VolumeFilter('totalVolGrows5D', 'totalVolume', 'growing', 5);
+export const totalVolDecreases5D = new VolumeFilter('totalVolDecreases5D', 'totalVolume', 'decreasing', 5);
 
 /* FILTERS SECTION: END */
