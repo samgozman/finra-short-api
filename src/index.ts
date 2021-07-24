d24eabc75cb3080f206ca0170cc9c73067ffde98
samgozman <sam@gozman.space>
2021-07-24 11:17:56 +0300

    Update isNotGarbage on start

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import app from './app';
+import { isNotGarbage } from './filter';
 import './utils/updater';
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log('Server is up on port ' + port);
+    await isNotGarbage.update();
 });
