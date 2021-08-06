9ed5f34bc1a8849e8809237d3bfa10b87885a8cd
samgozman <sam@gozman.space>
2021-08-06 10:32:49 +0300

    Test: stock router

---
+++ __tests__/routers/stockRouter.test.ts
@@ -0,0 +1,52 @@
+import supertest, { Response } from 'supertest';
+import { connection } from 'mongoose';
+
+import app from '../../src/app';
+import { userAdmin } from '../fixtures/db';
+import { db_fill } from '../fixtures/db_fill';
+import { User } from '../../src/models/User';
+import { IRoutersStock } from '../../src/routers/interface';
+import reportSample from '../fixtures/db_data/reportSample.json';
+
+// Correct body type for supertest response
+interface ResponseStock extends Response {
+    body: IRoutersStock;
+}
+
+beforeAll(async () => {
+    await db_fill();
+}, 30000);
+afterAll(async () => {
+    await connection.close();
+});
+
+test('Stock: should get stock on response', async () => {
+    const { token } = (await User.findOne({ login: userAdmin.login }))!;
+    const response: ResponseStock = await supertest(app)
+        .get('/stock?ticker=ZYNE&limit=21&sort=desc')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(200);
+    expect(response.body).toStrictEqual(reportSample);
+});
+
+test('Stock: should get 404 on error request', async () => {
+    const { token } = (await User.findOne({ login: userAdmin.login }))!;
+    await supertest(app)
+        .get('/stock?ticker=undefined')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(404);
+});
+
+test('Stock: should get 401 on wrong auth', async () => {
+    // Wrong auth
+    await supertest(app)
+        .get('/stock?ticker=ZYNE&limit=21&sort=desc')
+        .set('Authorization', 'Bearer 1234567890')
+        .send()
+        .expect(401);
+
+    // No auth
+    await supertest(app).get('/stock?ticker=ZYNE&limit=21&sort=desc').send().expect(401);
+});
