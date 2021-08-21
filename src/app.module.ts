094880aa00564a422d6bf95187848bc4b3a9816f
samgozman <sam@gozman.space>
2021-08-21 14:55:15 +0300

    *.env validation, Mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configValidationSchema } from './config.schema';
 import { UsersModule } from './users/users.module';
 import { StocksModule } from './stocks/stocks.module';
 import { VolumesModule } from './volumes/volumes.module';
@@ -14,6 +16,15 @@ import { CollectionModule } from './collection/collection.module';
 		ConfigModule.forRoot({
 			isGlobal: true,
 			envFilePath: `config/.${process.env.NODE_ENV}.env`,
+			validationSchema: configValidationSchema,
+		}),
+		MongooseModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => {
+				return {
+					uri: config.get('MONGODB_CONNECTION_URL'),
+				};
+			},
 		}),
 		UsersModule,
 		StocksModule,
