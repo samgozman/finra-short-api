199dc140f1163e40a06c0529c2102fc56c40ea0f
samgozman <sam@gozman.space>
2021-08-21 08:34:17 +0300

    Start as NestJS app

--- config/jestenv.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import dotenv from 'dotenv';
-
-// Setup env fot jest testing
-module.exports = async () => {
-    dotenv.config({ path: path.resolve(__dirname, 'test.env') });
-};
