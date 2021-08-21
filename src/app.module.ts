f6b301d38aada71c09e46e197e742621b3b69298
samgozman <sam@gozman.space>
2021-08-21 09:06:20 +0300

    Setup env and global validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,8 +10,28 @@ import { FiltersModule } from './filters/filters.module';
 import { CollectionModule } from './collection/collection.module';
 
 @Module({
-	imports: [UsersModule, StocksModule, VolumesModule, FiltersModule, CollectionModule],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: `config/.${process.env.NODE_ENV}.env`,
+		}),
+		UsersModule,
+		StocksModule,
+		VolumesModule,
+		FiltersModule,
+		CollectionModule,
+	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [
+		AppService,
+		// Apply this pipe on any request that flows into the application (instead of main.ts file)
+		{
+			provide: APP_PIPE,
+			useValue: new ValidationPipe({
+				// Check that incoming request don't have unexpected keys (removes them)
+				whitelist: true,
+			}),
+		},
+	],
 })
 export class AppModule {}
