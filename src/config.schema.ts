e1efadd7814d472ba7b116e87017e8872ddc0d74
samgozman <sam@gozman.space>
2021-08-22 10:29:47 +0300

    Change .env schema

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -7,6 +7,6 @@ export const configValidationSchema = Joi.object({
 		.default('mongodb://127.0.0.1:27017/finra-short-api')
 		.required(),
 	JWT_SECRET: Joi.string().required(),
-	ADMIN_KEY: Joi.string().required(),
+	COOKIE_KEY: Joi.string().required(),
 	SANDBOX_TOKEN: Joi.string().required(),
 });
