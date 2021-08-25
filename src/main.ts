90ec9caabdd99c100be6d8c0bb7cf3b41e9a3791
samgozman <sam@gozman.space>
2021-08-25 13:47:23 +0300

    Port config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import compression from 'compression';
 import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const port = app.get<ConfigService>(ConfigService).get('PORT');
 	app.use(helmet());
 	app.use(compression());
-	await app.listen(3000);
+	await app.listen(port);
 }
 bootstrap();
