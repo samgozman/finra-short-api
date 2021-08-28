e7aa0395c10472d4744c23f55cf4a88d3aed1820
samgozman <sam@gozman.space>
2021-08-28 18:11:19 +0300

    Unit testing: StocksController

--- src/models/stocks/stocks.controller.spec.ts
+++ src/models/stocks/stocks.controller.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service';
 import { StocksController } from './stocks.controller';
+import { StocksService } from './stocks.service';
+import { User } from '../users/schemas/user.schema';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('StocksController', () => {
 	let controller: StocksController;
 
+	const mockStock = {
+		ticker: 'AAPL',
+	};
+
+	class MockStocksService {
+		get() {
+			return Promise.resolve(mockStock);
+		}
+	}
+
+	const mockUser = {};
+	const mockUsersService = {};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [StocksController],
+			providers: [
+				{
+					provide: getModelToken(User.name),
+					useValue: mockUser,
+				},
+				{
+					provide: StocksService,
+					useClass: MockStocksService,
+				},
+				{
+					provide: UsersService,
+					useValue: mockUsersService,
+				},
+			],
 		}).compile();
 
 		controller = module.get<StocksController>(StocksController);
@@ -14,5 +45,14 @@ describe('StocksController', () => {
 
 	it('should be defined', () => {
 		expect(controller).toBeDefined();
+	});
+
+	it('should get stock', async () => {
+		const stock = await controller.getStock({
+			ticker: 'AAPL',
+			limit: 0,
+			sort: 'asc',
+		});
+		expect(stock).toEqual(mockStock);
 	});
 });
