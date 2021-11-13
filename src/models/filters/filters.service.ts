4ad8d8d1ad7578aa64967b124223ba27c7ac5146
samgozman <sam@gozman.space>
2021-11-13 09:12:00 +0300

    Back to Promise.all for filters generation

--- src/models/filters/filters.service.ts
+++ src/models/filters/filters.service.ts
@@ -199,9 +199,9 @@ export class FiltersService {
 			this.logger.warn('The filters regeneration process has started');
 			const updaters = this.getFiltersUpdaters();
 
-			for (const key in updaters) {
-				await updaters[key]();
-			}
+			await Promise.all(Object.values(updaters).map((e) => e())).catch(() => {
+				throw new Error('Error in filters generation process');
+			});
 
 			this.logger.warn('The filter collection has been successfully generated');
 
