58bf77ad9b1560b234a1b9d21f276c0bf10cc593
samgozman <sam@gozman.space>
2023-07-05 10:40:43 +0400

    Fix tabs

--- docker/testing/entrypoint.sh
+++ docker/testing/entrypoint.sh
@@ -8,12 +8,12 @@ set -e
 echo "TEST_MODE=$TEST_MODE"
 case $TEST_MODE in
 'e2e')
-	exec npm run test:e2e
-	;;
+  exec npm run test:e2e
+  ;;
 'e2e:watch')
-	exec npm run test:e2e:watch
-	;;
+  exec npm run test:e2e:watch
+  ;;
 *)
-	echo 'ERROR: Unrecognized TEST_MODE value'
-	;;
+  echo 'ERROR: Unrecognized TEST_MODE value'
+  ;;
 esac
