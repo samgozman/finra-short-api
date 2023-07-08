d49558a3db6b8df2bfda8e70f03f28946a7d24b7
samgozman <sam@gozman.space>
2023-07-08 12:43:57 +0400

    Add filters

--- src/stocks/stock.entity.ts
+++ src/stocks/stock.entity.ts
@@ -22,6 +22,8 @@ export class Stock {
   })
   ticker: string;
 
+  // Ratios (percentages)
+
   @Column({
     type: 'float',
     nullable: false,
@@ -59,6 +61,7 @@ export class Stock {
   shortExemptVolRatio20dAvg: number;
 
   // Numeric avg
+
   @Column({
     type: 'float',
     nullable: false,
@@ -112,4 +115,174 @@ export class Stock {
     nullable: false,
   })
   shortVol20dAvg: number;
+
+  // ? Presets filters
+
+  /** Filter new stocks with no data or incomplete */
+  @Column({
+    default: false,
+  })
+  isNotGarbage: boolean;
+
+  // ? In-line sequence of 5d
+
+  /** Short volume is growing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolGrows5D: boolean;
+
+  /** Short volume is decreasing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolDecreases5D: boolean;
+
+  /** Short volume ratio (%) is growing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolRatioGrows5D: boolean;
+
+  /** Short volume ratio (%) is decreasing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolRatioDecreases5D: boolean;
+
+  /** Total volume is growing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  totalVolGrows5D: boolean;
+
+  /** Total volume is decreasing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  totalVolDecreases5D: boolean;
+
+  /** Short Exempt volume is growing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolGrows5D: boolean;
+
+  /** Short Exempt volume is decreasing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolDecreases5D: boolean;
+
+  /** Short Exempt volume ratio is growing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolRatioGrows5D: boolean;
+
+  /** Short Exempt volume ratio is decreasing 5 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolRatioDecreases5D: boolean;
+
+  // ? In-line sequence of 3d
+
+  /** Short volume is growing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolGrows3D: boolean;
+
+  /** Short volume is decreasing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolDecreases3D: boolean;
+
+  /** Short volume ratio (%) is growing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolRatioGrows3D: boolean;
+
+  /** Short volume ratio (%) is decreasing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortVolRatioDecreases3D: boolean;
+
+  /** Total volume is growing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  totalVolGrows3D: boolean;
+
+  /** Total volume is decreasing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  totalVolDecreases3D: boolean;
+
+  /** Short Exempt volume is growing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolGrows3D: boolean;
+
+  /** Short Exempt volume is decreasing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolDecreases3D: boolean;
+
+  /** Short Exempt volume ratio is growing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolRatioGrows3D: boolean;
+
+  /** Short Exempt volume ratio is decreasing 3 days in a row */
+  @Column({
+    default: false,
+  })
+  shortExemptVolRatioDecreases3D: boolean;
+
+  // ? Abnormal volume
+
+  /** Abnormal short volume growing */
+  @Column({
+    default: false,
+  })
+  abnormalShortVolGrows: boolean;
+
+  /** Abnormal short volume decreasing */
+  @Column({
+    default: false,
+  })
+  abnormalShortVolDecreases: boolean;
+
+  /** Abnormal total volume growing */
+  @Column({
+    default: false,
+  })
+  abnormalTotalVolGrows: boolean;
+
+  /** Abnormal total volume decreasing */
+  @Column({
+    default: false,
+  })
+  abnormalTotalVolDecreases: boolean;
+
+  /** Abnormal short exempt volume growing */
+  @Column({
+    default: false,
+  })
+  abnormalShortExemptVolGrows: boolean;
+
+  /** Abnormal short exempt volume decreasing */
+  @Column({
+    default: false,
+  })
+  abnormalShortExemptVolDecreases: boolean;
 }
